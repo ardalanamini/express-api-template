975c0cf9d4a736ab4a9bc10bd45d1cc7f7036be5
Ardalan Amini <ardalanamini22@gmail.com>
2022-02-08 03:09:27 +0330

    deps: Add `cors` version `2.8.5`

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import router from "#src/router";
+import cors from "cors";
 import express from "express";
 import { parse } from "qs";
 
@@ -11,7 +12,13 @@ const server = express();
 server
   .disable("x-powered-by")
   .enable("trust proxy")
-  .set("query parser", parse);
+  .set("query parser", parse)
+  .set("json replacer fn", (key: string, value: unknown): unknown => key[0] === "_" ? undefined : value);
+
+/* ------------------------- SERVER MIDDLEWARES ------------------------- */
+
+server
+  .use(cors());
 
 /* ------------------------- SERVER ENDPOINTS ------------------------- */
 
