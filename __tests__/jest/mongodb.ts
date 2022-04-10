13dd1194d3ec1efe36b4055e5315748600f75e1c
Ardalan Amini <ardalanamini22@gmail.com>
2022-04-10 16:34:00 +0430

    test: Improve MongoDB Memory Server config

--- __tests__/jest/mongodb.ts
+++ __tests__/jest/mongodb.ts
@@ -1,8 +1,10 @@
 import { MongoMemoryReplSet } from "mongodb-memory-server";
+import * as path from "node:path";
 
 const MONGOD = new MongoMemoryReplSet({
   binary: {
     version: "5.0.6",
+    downloadDir: path.resolve(process.cwd(), "node_modules", ".cache", "mongodb-memory-server", "mongodb-binaries"),
   },
   replSet: {
     count: 1,
--- __tests__/jest/setup.ts
+++ __tests__/jest/setup.ts
@@ -1,7 +1,9 @@
-import { writeDBUri } from "./fs";
+import { writeDBUri, pruneDBBinaries } from "./fs";
 import MONGOD from "./mongodb";
 
 export default async function setup() {
+  pruneDBBinaries(MONGOD.binaryOpts);
+
   await MONGOD.start();
 
   await MONGOD.waitUntilRunning();
