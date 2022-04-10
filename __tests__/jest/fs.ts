55de15d6b747ee1a180e5f2376aaccecb2796387
Ardalan Amini <ardalanamini22@gmail.com>
2022-04-10 18:20:16 +0430

    test: Improve Jest config (#36)
    * test: Improve MongoDB Memory Server config
    
    * chore: Update NPM test script

--- __tests__/jest/fs.ts
+++ __tests__/jest/fs.ts
@@ -1,5 +1,5 @@
-import { mkdirSync, readFileSync, rmSync, writeFileSync } from "node:fs";
-import path from "node:path";
+import { mkdirSync, readdirSync, readFileSync, rmSync, writeFileSync, existsSync } from "node:fs";
+import * as path from "node:path";
 
 const DIRECTORY = path.resolve(process.cwd(), "node_modules", ".cache");
 const PATH = path.resolve(DIRECTORY, ".jest-mongodb");
@@ -18,4 +18,26 @@ export function readDBUri() {
 
 export function pruneDBUri() {
   rmSync(PATH);
+}
+
+export function pruneDBBinaries(options: { version?: string, downloadDir?: string }) {
+  const { downloadDir, version } = options;
+
+  if (downloadDir == null || version == null) return;
+
+  if (!existsSync(downloadDir)) return;
+
+  const binaries = readdirSync(downloadDir);
+
+  if (binaries.length === 0) return;
+
+  const oldBinaries = binaries.filter(binary => !binary.endsWith(version));
+
+  if (oldBinaries.length === 0) return;
+
+  if (binaries.length === oldBinaries.length) return rmSync(downloadDir, { recursive: true });
+
+  for (const binary of oldBinaries) {
+    rmSync(path.resolve(downloadDir, binary));
+  }
 }
